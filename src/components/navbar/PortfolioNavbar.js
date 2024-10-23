ed5f0cf906da7983442925ea8c2b21522c8b6fd1
Yingbo <liuyingbo81@hotmail.com>
2024-10-23 12:49:47 -0600

    last update

--- src/components/navbar/PortfolioNavbar.js
+++ src/components/navbar/PortfolioNavbar.js
@@ -3,14 +3,12 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { FaBars } from 'react-icons/fa';
 import './PortfolioNavbar.css';
 import { menus } from '../../utils/Constants';
-import IconWithCircle from '../icon/IconWithCircle';
 import IconWithRectangle from '../icon/IconWithRectangle';
 
-const PortfolioNavbar = ({activeSection, setActiveSection, setUserClicked }) => {
+const PortfolioNavbar = ({ activeSection, setActiveSection, setUserClicked }) => {
 
-  console.log("activeSection:"+activeSection);
+  console.log("activeSection:" + activeSection);
   const [expanded, setExpanded] = useState(false);
-
 
   const handleBtnClick = (id) => {
     setActiveSection(id);
@@ -18,33 +16,39 @@ const PortfolioNavbar = ({activeSection, setActiveSection, setUserClicked }) => 
     setTimeout(() => setUserClicked(false), 500);
 
     const section = document.getElementById(id);
-    if(section) {
+    if (section) {
       section.scrollIntoView({
         behavior: "smooth",
       });
     }
-  }
+  };
 
   const toggleNavbar = () => setExpanded(!expanded);
 
   return (
-    <div className="d-flex">
+    <div className="d-flex w-100">
+      {/* Full navbar for large screens */}
       <Navbar
         variant="dark"
         expand="lg"
         expanded={expanded}
-        className="navbar-denter"
+        className="navbar-denter d-none d-lg-flex"
       >
-        <Nav className="flex-row w-100">
-          {
-            menus.map((menu,index) => {
-              const {id, href, icon, textValue } = menu;
-              return (
-                <Nav.Link key={id} href={href} className="custom-nav-link"><IconWithRectangle iconName={icon} isActive={activeSection === id} onClick={()=>{handleBtnClick(icon, id)}} tooltipText={textValue}/></Nav.Link>
-              );
-            })
-          }
-          </Nav>
+        <Nav className="flex-row w-100 justify-content-around">
+          {menus.map((menu) => {
+            const { id, href, icon, textValue } = menu;
+            return (
+              <Nav.Link key={id} href={href} className="custom-nav-link">
+                <IconWithRectangle
+                  iconName={icon}
+                  isActive={activeSection === id}
+                  onClick={() => handleBtnClick(id)}
+                  tooltipText={textValue}
+                />
+              </Nav.Link>
+            );
+          })}
+        </Nav>
       </Navbar>
 
       {/* Hamburger menu for small screens */}
@@ -53,18 +57,18 @@ const PortfolioNavbar = ({activeSection, setActiveSection, setUserClicked }) => 
         variant="dark"
         expand="lg"
         expanded={expanded}
-        className="d-lg-none position-fixed"
-        style={{ width: '100%' }}
+        className="d-lg-none position-fixed w-100"
       >
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleNavbar}>
           <FaBars />
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-column">
-              <Nav.Link href="#home" onClick={() => setExpanded(false)}>Home</Nav.Link>
-              <Nav.Link href="#skills" onClick={() => setExpanded(false)}>Skills</Nav.Link>
-              <Nav.Link href="#experience" onClick={() => setExpanded(false)}>Experience</Nav.Link>
-              <Nav.Link href="#playground" onClick={() => setExpanded(false)}>Playground</Nav.Link>
+            <Nav.Link href="#home" onClick={() => setExpanded(false)}>Home</Nav.Link>
+            <Nav.Link href="#skills" onClick={() => setExpanded(false)}>Skills</Nav.Link>
+            <Nav.Link href="#experience" onClick={() => setExpanded(false)}>Experience</Nav.Link>
+            <Nav.Link href="#playground" onClick={() => setExpanded(false)}>Playground</Nav.Link>
+            <Nav.Link href="#contact" onClick={() => setExpanded(false)}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
